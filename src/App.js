30a4a10705c33d25f70ca29349fd10437ab3d9e1
Marcin <marcinbielen995@gmail.com>
2026-08-31 18:37:58 +0200

    Create home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
        <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
        </Routes>
       <Footer />
     </BrowserRouter>
