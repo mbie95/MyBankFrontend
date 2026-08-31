120b9a68c2ee4a0319fd210f9d8fa18d3a8b02d1
Marcin <marcinbielen995@gmail.com>
2026-08-31 13:07:56 +0200

    Create login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
        <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
        </Routes>
       <Footer />
     </BrowserRouter>
