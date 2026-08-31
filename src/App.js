c782e291991b790f1d0418f63f1104b6a4e27de3
Marcin <marcinbielen995@gmail.com>
2026-08-31 12:20:00 +0200

    Create register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+       <Routes>
+        <Route path="/register" element={<Register />} />
+       </Routes>
       <Footer />
     </BrowserRouter>
   );
