e6a792b8ffa319309cb7101ca840be0c278c6e7c
Marcin <marcinbielen995@gmail.com>
2026-09-03 13:41:05 +0200

    Create profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
 
         <Route path="*" element={<NotFound />} />
        </Routes>
