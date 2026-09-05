ba06c65eeb3dbe4b3911288c8927263962b7fc3a
Marcin <marcinbielen995@gmail.com>
2026-09-05 14:14:40 +0200

    Create transactions page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Profile from "./pages/Profile";
 import UpdateProfile from "./pages/UpdateProfile";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import Transactions from "./pages/Transactions";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="/update-profile" element={<UpdateProfile />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/transactions" element={<Transactions />} />
 
         <Route path="*" element={<NotFound />} />
        </Routes>
